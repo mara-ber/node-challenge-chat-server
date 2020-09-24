414d0b227824f6660a8a66f9a857d971f546dbfd
Marina <mara.berezin@gmail.com>
2020-09-24 16:03:50 +0100

    level 1

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.json());
+
+
 const welcomeMessage = {
   id: 0,
   from: "Bart",
@@ -20,4 +23,30 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", function (req, res) {
+  res.send(messages);
+});
+
+app.get("/messages/:messageId", function (req, res) {
+  const id = Number(req.params.messageId);
+  const message = messages.find((message) => message.id === id);
+  res.send(message);
+});
+
+app.delete("/messages/:messageId", function (req, res) {
+  const id = Number(req.params.messageId);
+  const messageToDel = messages.find((message) => message.id === id);
+  messages.splice(messages.indexOf(messageToDel), 1);
+  res.send(messageToDel);
+});
+
+
+app.post("/messages", function (req, res) {
+  const newMessage = req.body;
+  messages.push(newMessage);
+  res.send(newMessage);
+});
+
+app.listen(3100)
+
+//app.listen(process.env.PORT);
